81aa579be41f6b2eb6f03830b7c419f9eebdb809
Joe-metry <joemetry150@gmail.com>
2023-09-28 13:38:13 +0000

    Iterator Through Object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,3 @@
-export default function* createIteratorObject(report) {
-  const departments = Object.keys(report.allEmployees);
-
-  for (const department of departments) {
-    const employees = report.allEmployees[department];
-
-    for (const employee of employees) {
-      yield employee;
-    }
-  }
+export default function iterateThroughObject(reportWithIterator) {
+  return reportWithIterator.join(' | ');
 }
